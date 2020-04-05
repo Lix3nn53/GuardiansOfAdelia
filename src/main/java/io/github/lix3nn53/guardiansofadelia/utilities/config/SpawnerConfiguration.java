3b2b7e1ad30ce8b802f4ff0a31bc7c6809175881
Lix3nn53 <lixakyildiz@gmail.com>
2020-04-05 18:44:25 +0300

    Move and fix spawner configs

--- src/main/java/io/github/lix3nn53/guardiansofadelia/utilities/config/SpawnerConfiguration.java
+++ src/main/java/io/github/lix3nn53/guardiansofadelia/utilities/config/SpawnerConfiguration.java
@@ -15,58 +15,86 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class SpawnerConfiguration {
 
-    private static FileConfiguration spawnersConfig;
-    private static FileConfiguration dungeonSpawnersConfig;
+    private static HashMap<String, FileConfiguration> worldNameToConfiguration = new HashMap<>();
 
     static void createConfigs() {
-        createDungeonSpawners();
-        createSpawners();
+        createSpawners("world");
+        createSpawners("dungeons");
     }
 
     static void loadConfigs() {
-        loadDungeonSpawners();
-        loadSpawners();
+        loadSpawners("world");
+        loadSpawners("dungeons");
     }
 
     static void writeConfigs() {
-        writeDungeonSpawners();
-        writeSpawners();
+        writeSpawners("world");
+        writeSpawners("dungeons");
     }
 
-    private static void createSpawners() {
-        File customConfigFile = new File(ConfigManager.DATA_FOLDER, "spawners.yml");
+    private static void createSpawners(String worldName) {
+        String fileName = worldName + ".yml";
+        String filePath = ConfigManager.DATA_FOLDER + File.separator + "spawners";
+        File customConfigFile = new File(filePath, fileName);
         if (!customConfigFile.exists()) {
             customConfigFile.getParentFile().mkdirs();
-            GuardiansOfAdelia.getInstance().saveResource("spawners.yml", false);
+
+            try {
+                customConfigFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        spawnersConfig = new YamlConfiguration();
+        YamlConfiguration yamlConfiguration = new YamlConfiguration();
         try {
-            spawnersConfig.load(customConfigFile);
+            yamlConfiguration.load(customConfigFile);
+            worldNameToConfiguration.put(worldName, yamlConfiguration);
         } catch (IOException | InvalidConfigurationException e) {
             e.printStackTrace();
         }
     }
 
-    private static void loadSpawners() {
-        int spawnerNumber = spawnersConfig.getInt("SpawnerNumber");
+    private static void loadSpawners(String worldName) {
+        FileConfiguration spawnersConfig = worldNameToConfiguration.get(worldName);
+
+        int spawnerNumber = spawnersConfig.getInt("SpawnerCount");
+        int debug = 0;
         for (int i = 1; i <= spawnerNumber; i++) {
-            String worldString = spawnersConfig.getString("Spawners.s" + i + ".world");
-            if (worldString == null) {
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "Spawner string null");
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "index: " + i);
+            World world = Bukkit.getWorld(worldName);
+
+            if (!spawnersConfig.contains("Spawners.s" + i + ".x")) {
+                debugError(worldName, i);
                 continue;
             }
 
-            World world = Bukkit.getWorld(worldString);
+            if (!spawnersConfig.contains("Spawners.s" + i + ".y")) {
+                debugError(worldName, i);
+                continue;
+            }
 
-            if (world == null) {
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "Spawner string not valid");
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "index: " + i);
+            if (!spawnersConfig.contains("Spawners.s" + i + ".z")) {
+                debugError(worldName, i);
+                continue;
+            }
+
+            if (!spawnersConfig.contains("Spawners.s" + i + ".mob")) {
+                debugError(worldName, i);
+                continue;
+            }
+
+            if (!spawnersConfig.contains("Spawners.s" + i + ".amount")) {
+                debugError(worldName, i);
+                continue;
+            }
+
+            if (!spawnersConfig.contains("Spawners.s" + i + ".maxamount")) {
+                debugError(worldName, i);
                 continue;
             }
 
@@ -80,13 +108,19 @@ public class SpawnerConfiguration {
             int maxamount = spawnersConfig.getInt("Spawners.s" + i + ".maxamount");
             Spawner spawner = new Spawner(location, adeliaEntity, amount, maxamount);
             SpawnerManager.addSpawner(spawner);
+            debug++;
         }
+        GuardiansOfAdelia.getInstance().getLogger().info("Loaded " + debug + " spawners for world: " + worldName);
     }
 
-    private static void writeSpawners() {
+    private static void writeSpawners(String worldName) {
+        YamlConfiguration spawnersConfig = new YamlConfiguration();
+
         int i = 1;
         List<Spawner> spawnerList = SpawnerManager.getSpawners();
+
         int nonIncludedCount = 0;
+
         for (Spawner spawner : spawnerList) {
             Location location = spawner.getLocation();
             if (location.getWorld() == null) {
@@ -94,102 +128,34 @@ public class SpawnerConfiguration {
                 continue;
             }
 
-            if (location.getWorld().getName().equals("dungeons")) { //only dungeon spawners are seperate
+            if (!location.getWorld().getName().equals(worldName)) {
                 nonIncludedCount++;
                 continue;
             }
+
             spawnersConfig.set("Spawners.s" + i + ".x", location.getX());
             spawnersConfig.set("Spawners.s" + i + ".y", location.getY());
             spawnersConfig.set("Spawners.s" + i + ".z", location.getZ());
-            spawnersConfig.set("Spawners.s" + i + ".world", location.getWorld().getName());
             spawnersConfig.set("Spawners.s" + i + ".mob", spawner.getAdeliaEntity().getAdeliaEntityKey());
             spawnersConfig.set("Spawners.s" + i + ".amount", spawner.getAmountPerSpawn());
             spawnersConfig.set("Spawners.s" + i + ".maxamount", spawner.getMaxAmount());
             i++;
         }
-        spawnersConfig.set("SpawnerNumber", spawnerList.size() - nonIncludedCount);
+        spawnersConfig.set("SpawnerCount", spawnerList.size() - nonIncludedCount);
+
         try {
-            spawnersConfig.save(ConfigManager.DATA_FOLDER + "/spawners.yml");
+            String fileName = worldName + ".yml";
+            String filePath = ConfigManager.DATA_FOLDER + File.separator + "spawners";
+
+            spawnersConfig.save(filePath + File.separator + fileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void createDungeonSpawners() {
-        File customConfigFile = new File(ConfigManager.DATA_FOLDER, "dungeonSpawners.yml");
-        if (!customConfigFile.exists()) {
-            customConfigFile.getParentFile().mkdirs();
-            GuardiansOfAdelia.getInstance().saveResource("dungeonSpawners.yml", false);
-        }
-
-        dungeonSpawnersConfig = new YamlConfiguration();
-        try {
-            dungeonSpawnersConfig.load(customConfigFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void loadDungeonSpawners() {
-        int spawnerNumber = dungeonSpawnersConfig.getInt("SpawnerNumber");
-        for (int i = 1; i <= spawnerNumber; i++) {
-            String worldString = dungeonSpawnersConfig.getString("Spawners.s" + i + ".world");
-            if (worldString == null) {
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "DungeonSpawner string null");
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "index: " + i);
-                continue;
-            }
-
-            World world = Bukkit.getWorld(worldString);
-
-            if (world == null) {
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "DungeonSpawner string not valid");
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "index: " + i);
-                continue;
-            }
-
-            double x = dungeonSpawnersConfig.getDouble("Spawners.s" + i + ".x");
-            double y = dungeonSpawnersConfig.getDouble("Spawners.s" + i + ".y");
-            double z = dungeonSpawnersConfig.getDouble("Spawners.s" + i + ".z");
-            Location location = new Location(world, x, y, z);
-            String adeliaEntityString = dungeonSpawnersConfig.getString("Spawners.s" + i + ".mob");
-            AdeliaEntity adeliaEntity = AdeliaEntityManager.getEntity(adeliaEntityString);
-            int amount = dungeonSpawnersConfig.getInt("Spawners.s" + i + ".amount");
-            int maxamount = dungeonSpawnersConfig.getInt("Spawners.s" + i + ".maxamount");
-            Spawner spawner = new Spawner(location, adeliaEntity, amount, maxamount);
-            SpawnerManager.addSpawner(spawner);
-        }
-    }
-
-    private static void writeDungeonSpawners() {
-        int i = 1;
-        List<Spawner> spawnerList = SpawnerManager.getSpawners();
-        int nonIncludedCount = 0;
-        for (Spawner spawner : spawnerList) {
-            Location location = spawner.getLocation();
-            if (location.getWorld() == null) {
-                GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "DUNGEON SPAWNER WRITE NULL WORLD");
-                continue;
-            }
-
-            if (!location.getWorld().getName().equals("dungeons")) {
-                nonIncludedCount++;
-                continue;
-            }
-            dungeonSpawnersConfig.set("Spawners.s" + i + ".x", location.getX());
-            dungeonSpawnersConfig.set("Spawners.s" + i + ".y", location.getY());
-            dungeonSpawnersConfig.set("Spawners.s" + i + ".z", location.getZ());
-            dungeonSpawnersConfig.set("Spawners.s" + i + ".world", location.getWorld().getName());
-            dungeonSpawnersConfig.set("Spawners.s" + i + ".mob", spawner.getAdeliaEntity().getAdeliaEntityKey());
-            dungeonSpawnersConfig.set("Spawners.s" + i + ".amount", spawner.getAmountPerSpawn());
-            dungeonSpawnersConfig.set("Spawners.s" + i + ".maxamount", spawner.getMaxAmount());
-            i++;
-        }
-        dungeonSpawnersConfig.set("SpawnerNumber", spawnerList.size() - nonIncludedCount);
-        try {
-            dungeonSpawnersConfig.save(ConfigManager.DATA_FOLDER + "/dungeonSpawners.yml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static void debugError(String worldName, int i) {
+        GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "Spawner config not valid");
+        GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "worldName: " + worldName);
+        GuardiansOfAdelia.getInstance().getLogger().info(ChatColor.RED + "index: " + i);
     }
 }
